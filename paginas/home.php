81b52fb52b318ac965442433bd7e35fdd0a1d468
RootViolence <guilhermemendes.trabalho@gmail.com>
2024-06-24 02:39:39 -0300

    Colocando card-box dos jogos, imagens exemplo, configuraçao do SASS, melhorando carrosel.

--- paginas/home.php
+++ paginas/home.php
@@ -1,5 +1,5 @@
 
-    
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <link rel="stylesheet" href="css/style.css">
     <!-- STYLESHEET -->
@@ -10,9 +10,9 @@
     <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
     <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
   </ol>
-  <div class="carousel-inner col-5 container mt-1">
+  <div class="carousel-inner col-8 container mt-1">
     <div class="carousel-item active">
-      <img class="d-block w-100" src="imagens/Jogos/imagem1.jpg" alt="Primeiro Slide">
+      <img class="d-block w-100" src="imagens/Jogos/sifu.jpeg" alt="Primeiro Slide">
     </div>
     <div class="carousel-item">
       <img class="d-block w-100" src="imagens/Jogos/imagem2.jpg" alt="Segundo Slide">
@@ -21,19 +21,58 @@
       <img class="d-block w-100" src="imagens/Jogos/imagem3.png" alt="Terceiro Slide">
     </div>
   </div>
-  <a class="carousel-control-prev bg-dark" href="#carouselExampleIndicators" role="button" data-slide="prev">
+  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
     <span class="carousel-control-prev-icon" aria-hidden="true"></span>
     <span class="sr-only">Anterior</span>
   </a>
-  <a class="carousel-control-next bg-dark" href="#carouselExampleIndicators" role="button" data-slide="next">
+  <a class="carousel-control-next " href="#carouselExampleIndicators" role="button" data-slide="next">
     <span class="carousel-control-next-icon " aria-hidden="true"></span>
     <span class="sr-only">Próximo</span>
   </a>
 </div>
 <section id="preto">
-<h2 class="container-fluid text-center">NOSSOS JOGOS:</h2>
+<h2 class="container-fluid text-center" style="color:#640b64">MAIS JOGADOS</h2>
 </section>
 
+
+<div class="row row-cols-1 row-cols-md-3 g-4 m-5">
+  <div class="col">
+    <div class="card h-100 container-fluid bg-dark">
+      <img src="imagens/jogos/eldenring.jpeg" class="card-img-top mx-auto mt-2" alt="...">
+      <div class="card-body">
+        <h5 class="card-title" style="color:#BEBEBE">Dark Souls REMASTERED</h5>
+        <p class="card-text" style="color:#BEBEBE;">Experimente novamente o jogo aclamado pela crítica e definidor de gênero que foi o início de tudo. Belamente remasterizado, volte a Lordran com detalhes impressionantes</p>
+      </div>
+      <div class="card-footer">
+        <small class="text-body-secondary">Last updated 3 mins ago</small>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card h-100 container-fluid bg-dark">
+      <img src="imagens/jogos/sifuicon.jpg" class="card-img-top mx-auto mt-2" alt="...">
+      <div class="card-body">
+        <h5 class="card-title" style="color:#BEBEBE">Sifu</h5>
+        <p class="card-text" style="color:#BEBEBE">Dos mesmos criadores de Absolver, o aclamado jogo de luta para PS4, desta vez a Sloclap nos brinda com Sifu, onde acompanhamos a vingança de um jovem aprendiz de kung fu contra os assassinos de sua família. É um contra todos, sem nenhum aliado e com inúmeros inimigos.</p>
+      </div>
+      <div class="card-footer">
+        <small class="text-body-secondary"">Last updated 3 mins ago</small>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card h-100 container-fluid bg-dark">
+      <img src="imagens/jogos/ABSOLVER.PNG" class="card-img-top mx-auto mt-2" alt="...">
+      <div class="card-body">
+        <h5 class="card-title" style="color:#BEBEBE;">Absolver</h5>
+        <p class="card-text" style="color:#BEBEBE;">Absolver é um jogo de combate multijogador online, onde você usa uma máscara para ser avaliado se é um Candidato digno de entrar na força de elite dos onipresentes Guias, que criaram sua máscara e são os novos governantes do antigo Império de Adal.</p>
+      </div>
+      <div class="card-footer">
+        <small class="text-body-secondary" style="color:#BEBEBE;">Last updated 3 mins ago</small>
+      </div>
+    </div>
+  </div>
+</div>
 
 
 
