6b54a9a6041a8112f05241be3264199ab2356cce
RootViolence <guilhermemendes.trabalho@gmail.com>
2024-06-24 02:45:47 -0300

    Atualizaçao grande, colocando fotos e imagens da equipe na pagina equipe, colocando e estilizando aba contate-nos, botao para abrir mais conteudo na pagina home. Padronizando os botoes e esestilizando o geral do site

--- index.php
+++ index.php
@@ -5,10 +5,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>SK GAMES</title>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+        crossorigin="anonymous"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="css/style.css">
-    <link rel="icon shortcut" href="">
+    <link rel="icon shortcut" href="imagens/logos/TachPlayINVETIDO.png">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap" rel="stylesheet">
@@ -16,12 +19,12 @@
 </head>
 
 <body>
-    <header class="header">
-        <nav class="nav">
+    <header class="header mx-auto">
+        <nav class="nav ">
 
             <a class="logo" href="home">SK GAMES</a>
 
-            <ul class="nav-list">   
+            <ul class="nav-list">
 
                 <li><a href="equipe">EQUIPE</a></li>
                 <li><a href="home">GAMES</a></li>
@@ -56,7 +59,8 @@
 </body>
 <footer class="text-center container-fluid">
 
-    <p>Desenvolvido Por : <a href="#" target="_blank">Guilherme Mendes</a></p>
+    <p class="text-white">Desenvolvido Por : <a href="https://github.com/RootViolence" target="_blank">Guilherme
+            Mendes</a></p>
 
 </footer>
 
--- paginas/home.php
+++ paginas/home.php
@@ -1,5 +1,4 @@
-
-  <!-- Arquivos CSS do Bootstrap -->
+<!-- Arquivos CSS do Bootstrap -->
 <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
 
 <!-- Arquivos JavaScript do Bootstrap (jQuery primeiro, depois Popper.js e por último o Bootstrap) -->
@@ -7,11 +6,13 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    <link rel="stylesheet" href="css/style.css">
-    
-    <!-- STYLESHEET -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+  integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+  integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+<link rel="stylesheet" href="css/style.css">
+
+<!-- STYLESHEET -->
 
 <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
   <ol class="carousel-indicators">
@@ -40,20 +41,23 @@
   </a>
 </div>
 <section id="preto">
-<h2 class="container-fluid text-center" style="color:#640b64">MAIS JOGADOS</h2>
+  <h2 class="container-fluid text-center mt-3" style="color:#640b64">MAIS JOGADOS</h2>
 </section>
 
+<!-- CARDBOX -->
 
 <div class="row row-cols-1 row-cols-md-3 g-4 m-3">
   <div class="col">
     <div class="card h-100 container-fluid bg-dark">
       <img src="imagens/jogos/eldenring.jpeg" class="card-img-top mx-auto mt-2" alt="...">
       <div class="card-body">
-        <h5 class="card-title" style="color:#BEBEBE">Dark Souls REMASTERED</h5>
-        <p class="card-text" style="color:#BEBEBE;">Experimente novamente o jogo aclamado pela crítica e definidor de gênero que foi o início de tudo. Belamente remasterizado, volte a Lordran com detalhes impressionantes</p>
+        <h5 class="card-title text-white">Dark Souls REMASTERED</h5>
+        <p class="card-text" style="color:#BEBEBE;">Experimente novamente o jogo aclamado pela crítica e definidor de
+          gênero que foi o início de tudo. Belamente remasterizado, volte a Lordran com detalhes impressionantes</p>
       </div>
       <div class="card-footer">
-        <small class="text-body-secondary"style="background-color:purple;">Desenvolvedores: FromSoftware, Bandai Namco Entertainment, Bluepoint Games, Virtuos, Japan Studio, QLOC</small>
+        <small class="text-body-secondary text-white">Desenvolvedores: FromSoftware, Bandai Namco Entertainment,
+          Bluepoint Games, Virtuos, Japan Studio, QLOC</small>
       </div>
     </div>
   </div>
@@ -61,11 +65,13 @@
     <div class="card h-100 container-fluid bg-dark">
       <img src="imagens/jogos/sifuicon.jpg" class="card-img-top mx-auto mt-2" alt="...">
       <div class="card-body">
-        <h5 class="card-title" style="color:#BEBEBE">Sifu</h5>
-        <p class="card-text" style="color:#BEBEBE">Dos mesmos criadores de Absolver, o aclamado jogo de luta para PS4, desta vez a Sloclap nos brinda com Sifu, onde acompanhamos a vingança de um jovem aprendiz de kung fu contra os assassinos de sua família. É um contra todos, sem nenhum aliado e com inúmeros inimigos.</p>
+        <h5 class="card-title text-white">Sifu</h5>
+        <p class="card-text" style="color:#BEBEBE">Dos mesmos criadores de Absolver, o aclamado jogo de luta para PS4,
+          desta vez a Sloclap nos brinda com Sifu, onde acompanhamos a vingança de um jovem aprendiz de kung fu contra
+          os assassinos de sua família. É um contra todos, sem nenhum aliado e com inúmeros inimigos.</p>
       </div>
       <div class="card-footer">
-        <small class="text-body-secondary"style="background-color:purple;">Desenvolvedores: Sloclap, H2 Interactive</small>
+        <small class="text-body-secondary text-white">Desenvolvedores: Sloclap, H2 Interactive</small>
       </div>
     </div>
   </div>
@@ -73,11 +79,164 @@
     <div class="card h-100 container-fluid bg-dark">
       <img src="imagens/jogos/ABSOLVER.PNG" class="card-img-top mx-auto mt-2" alt="...">
       <div class="card-body">
-        <h5 class="card-title" style="color:#BEBEBE;">Absolver</h5>
-        <p class="card-text" style="color:#BEBEBE;">Absolver é um jogo de combate multijogador online, onde você usa uma máscara para ser avaliado se é um Candidato digno de entrar na força de elite dos onipresentes Guias, que criaram sua máscara e são os novos governantes do antigo Império de Adal.</p>
+        <h5 class="card-title text-white">Absolver</h5>
+        <p class="card-text" style="color:#BEBEBE;">Absolver é um jogo de combate multijogador online, onde você usa uma
+          máscara para ser avaliado se é um Candidato digno de entrar na força de elite dos onipresentes Guias, que
+          criaram sua máscara e são os novos governantes do antigo Império de Adal.</p>
       </div>
       <div class="card-footer">
-        <small class="text-body-secondary" style="background-color:purple;">Desenvolvedores: Sloclap</small>
+        <small class="text-body-secondary text-white">Desenvolvedores: Sloclap</small>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+<div class="row row-cols-1 row-cols-md-3 g-4 m-3">
+  <div class="col">
+    <div class="card h-100 container-fluid bg-dark">
+      <img src="imagens/jogos/eldenring.jpeg" class="card-img-top mx-auto mt-2" alt="...">
+      <div class="card-body">
+        <h5 class="card-title text-white">Dark Souls REMASTERED</h5>
+        <p class="card-text" style="color:#BEBEBE;">Experimente novamente o jogo aclamado pela crítica e definidor de
+          gênero que foi o início de tudo. Belamente remasterizado, volte a Lordran com detalhes impressionantes</p>
+      </div>
+      <div class="card-footer">
+        <small class="text-body-secondary text-white">Desenvolvedores: FromSoftware, Bandai Namco Entertainment,
+          Bluepoint Games, Virtuos, Japan Studio, QLOC</small>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card h-100 container-fluid bg-dark">
+      <img src="imagens/jogos/sifuicon.jpg" class="card-img-top mx-auto mt-2" alt="...">
+      <div class="card-body">
+        <h5 class="card-title text-white">Sifu</h5>
+        <p class="card-text" style="color:#BEBEBE">Dos mesmos criadores de Absolver, o aclamado jogo de luta para PS4,
+          desta vez a Sloclap nos brinda com Sifu, onde acompanhamos a vingança de um jovem aprendiz de kung fu contra
+          os assassinos de sua família. É um contra todos, sem nenhum aliado e com inúmeros inimigos.</p>
+      </div>
+      <div class="card-footer">
+        <small class="text-body-secondary text-white">Desenvolvedores: Sloclap, H2 Interactive</small>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card h-100 container-fluid bg-dark">
+      <img src="imagens/jogos/ABSOLVER.PNG" class="card-img-top mx-auto mt-2" alt="...">
+      <div class="card-body">
+        <h5 class="card-title text-white">Absolver</h5>
+        <p class="card-text" style="color:#BEBEBE;">Absolver é um jogo de combate multijogador online, onde você usa uma
+          máscara para ser avaliado se é um Candidato digno de entrar na força de elite dos onipresentes Guias, que
+          criaram sua máscara e são os novos governantes do antigo Império de Adal.</p>
+      </div>
+      <div class="card-footer">
+        <small class="text-body-secondary text-white">Desenvolvedores: Sloclap</small>
+      </div>
+    </div>
+  </div>
+</div>
+
+<!-- BOTAO ESCONDER -->
+<div class=" mb-5 center-screen">
+  <button class="btn btn-primary text-center mt-5 mb-2 bg-purple" type="button" data-toggle="collapse"
+    data-target="#conteudoCollapse" aria-expanded="false" aria-controls="conteudoCollapse">
+    Abrir/Fechar Conteúdo
+  </button>
+</div>
+
+<!-- ESCONDER -->
+<div class="collapse" id="conteudoCollapse">
+  <div class="card card-body" style="background-color:black;">
+    <div class="row row-cols-1 row-cols-md-3 g-4 m-3">
+      <div class="col">
+        <div class="card h-100 container-fluid bg-dark">
+          <img src="imagens/jogos/eldenring.jpeg" class="card-img-top mx-auto mt-2" alt="...">
+          <div class="card-body">
+            <h5 class="card-title text-white">Dark Souls REMASTERED</h5>
+            <p class="card-text" style="color:#BEBEBE;">Experimente novamente o jogo aclamado pela crítica e definidor
+              de gênero que foi o início de tudo. Belamente remasterizado, volte a Lordran com detalhes impressionantes
+            </p>
+          </div>
+          <div class="card-footer">
+            <small class="text-body-secondary text-white">Desenvolvedores: FromSoftware, Bandai Namco Entertainment,
+              Bluepoint Games, Virtuos, Japan Studio, QLOC</small>
+          </div>
+        </div>
+      </div>
+      <div class="col">
+        <div class="card h-100 container-fluid bg-dark">
+          <img src="imagens/jogos/sifuicon.jpg" class="card-img-top mx-auto mt-2" alt="...">
+          <div class="card-body">
+            <h5 class="card-title text-white">Sifu</h5>
+            <p class="card-text" style="color:#BEBEBE">Dos mesmos criadores de Absolver, o aclamado jogo de luta para
+              PS4, desta vez a Sloclap nos brinda com Sifu, onde acompanhamos a vingança de um jovem aprendiz de kung fu
+              contra os assassinos de sua família. É um contra todos, sem nenhum aliado e com inúmeros inimigos.</p>
+          </div>
+          <div class="card-footer">
+            <small class="text-body-secondary text-white">Desenvolvedores: Sloclap, H2 Interactive</small>
+          </div>
+        </div>
+      </div>
+      <div class="col">
+        <div class="card h-100 container-fluid bg-dark">
+          <img src="imagens/jogos/ABSOLVER.PNG" class="card-img-top mx-auto mt-2" alt="...">
+          <div class="card-body">
+            <h5 class="card-title text-white">Absolver</h5>
+            <p class="card-text" style="color:#BEBEBE;">Absolver é um jogo de combate multijogador online, onde você usa
+              uma máscara para ser avaliado se é um Candidato digno de entrar na força de elite dos onipresentes Guias,
+              que criaram sua máscara e são os novos governantes do antigo Império de Adal.</p>
+          </div>
+          <div class="card-footer">
+            <small class="text-body-secondary text-white">Desenvolvedores: Sloclap</small>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="row row-cols-1 row-cols-md-3 g-4 m-3">
+      <div class="col">
+        <div class="card h-100 container-fluid bg-dark">
+          <img src="imagens/jogos/eldenring.jpeg" class="card-img-top mx-auto mt-2" alt="...">
+          <div class="card-body">
+            <h5 class="card-title text-white">Dark Souls REMASTERED</h5>
+            <p class="card-text" style="color:#BEBEBE;">Experimente novamente o jogo aclamado pela crítica e definidor
+              de gênero que foi o início de tudo. Belamente remasterizado, volte a Lordran com detalhes impressionantes
+            </p>
+          </div>
+          <div class="card-footer">
+            <small class="text-body-secondary text-white">Desenvolvedores: FromSoftware, Bandai Namco Entertainment,
+              Bluepoint Games, Virtuos, Japan Studio, QLOC</small>
+          </div>
+        </div>
+      </div>
+      <div class="col">
+        <div class="card h-100 container-fluid bg-dark">
+          <img src="imagens/jogos/sifuicon.jpg" class="card-img-top mx-auto mt-2" alt="...">
+          <div class="card-body">
+            <h5 class="card-title text-white">Sifu</h5>
+            <p class="card-text" style="color:#BEBEBE">Dos mesmos criadores de Absolver, o aclamado jogo de luta para
+              PS4, desta vez a Sloclap nos brinda com Sifu, onde acompanhamos a vingança de um jovem aprendiz de kung fu
+              contra os assassinos de sua família. É um contra todos, sem nenhum aliado e com inúmeros inimigos.</p>
+          </div>
+          <div class="card-footer">
+            <small class="text-body-secondary text-white">Desenvolvedores: Sloclap, H2 Interactive</small>
+          </div>
+        </div>
+      </div>
+      <div class="col">
+        <div class="card h-100 container-fluid bg-dark">
+          <img src="imagens/jogos/ABSOLVER.PNG" class="card-img-top mx-auto mt-2" alt="...">
+          <div class="card-body">
+            <h5 class="card-title text-white">Absolver</h5>
+            <p class="card-text" style="color:#BEBEBE;">Absolver é um jogo de combate multijogador online, onde você usa
+              uma máscara para ser avaliado se é um Candidato digno de entrar na força de elite dos onipresentes Guias,
+              que criaram sua máscara e são os novos governantes do antigo Império de Adal.</p>
+          </div>
+          <div class="card-footer">
+            <small class="text-body-secondary text-white">Desenvolvedores: Sloclap</small>
+          </div>
+        </div>
       </div>
     </div>
   </div>
@@ -98,5 +257,5 @@
 
 
 
-
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+  integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
